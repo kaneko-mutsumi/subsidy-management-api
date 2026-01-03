b6714db6fcdbc0d2f91f415cfab239f044b12655
kaneko-mutsumi <oto.010wz@gmail.com>
2026-01-03 22:20:24 +0900

    chore: bootstrap Spring Boot project

---
+++ src/main/java/com/example/subsidy_management_api/SubsidyManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.subsidy_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubsidyManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubsidyManagementApiApplication.class, args);
+	}
+
+}
